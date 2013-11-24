accd95102540a884f7d169fd866453be217b839b
andycsoto <andres.c.soto@gmail.com>
2013-11-24 20:49:05 -0300

    change in settings

--- indtienda/settings.py
+++ indtienda/settings.py
@@ -12,16 +12,17 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+    ('Andres Cifuentes', 'andres.c.soto@gmail.com')
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'indtiendadb',                      # Or path to database file if using sqlite3.
+        'USER': 'andycsoto',                      # Not used with sqlite3.
+        'PASSWORD': 'aacs1988',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -67,7 +68,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/andycsoto/webapps/staticapp/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -135,9 +136,9 @@ INSTALLED_APPS = [
     'south',
     'compressor',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 ] + get_core_apps()
 
 # A sample logging configuration. The only tangible logging
@@ -195,6 +196,16 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
     },
 }
+
+EMAIL_HOST = 'smtp.webfaction.com'
+
+EMAIL_HOST_USER = 'andycsoto'
+
+EMAIL_HOST_PASSWORD = 'aacs1988'
+
+DEFAULT_FROM_EMAIL = 'andycsoto@webmaster.cl'
+
+SERVER_EMAIL = 'andycsoto@webmaster.cl'
 
 try:
     from local_settings import *
