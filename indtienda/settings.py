9093494d9472f25dc96308f08c2c7c1ec4eb9457
andycsoto <andres.c.soto@gmail.com>
2013-12-10 16:56:40 -0300

    cambie el directorio media

--- indtienda/settings.py
+++ indtienda/settings.py
@@ -59,7 +59,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = location("public/media")
+MEDIA_ROOT = "/home/andycsoto/webapps/indtienda/public/media"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
