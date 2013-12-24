cf63ba26ec9077607696b983e3c64f75ef44fd93
andycsoto <andres.c.soto@gmail.com>
2013-12-24 00:18:00 -0300

    Cambie media URL y media ROOT para poder mostrar imagenes.

--- indtienda/settings.py
+++ indtienda/settings.py
@@ -64,13 +64,13 @@ MEDIA_ROOT = location("media")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'andycsoto.webfactional.com/static'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/andycsoto/webapps/static_media/'
+STATIC_ROOT = '/home/andycsoto/webapps/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
