f9b80fd4294a96c2fe0f3ed8e96e83973a362617
Andres Cifuentes <acifuentes@lawlogix.com>
2013-12-24 12:42:55 -0300

    modified urls for images in settings

--- indtienda/settings.py
+++ indtienda/settings.py
@@ -65,7 +65,7 @@ MEDIA_ROOT = '/home/andycsoto/webapps/static_media/media'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'andycsoto.webfactional.com/media'
+MEDIA_URL = 'andycsoto.webfactional.com/static/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
