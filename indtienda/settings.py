0b65a1e1db514cace1d3648a23426fb313851612
andycsoto <andres.c.soto@gmail.com>
2014-01-02 21:31:15 -0300

    corregida db settings

--- indtienda/settings.py
+++ indtienda/settings.py
@@ -19,14 +19,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite3',
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'indtiendadb',                      # Or path to database file if using sqlite3.
-#        'USER': 'andycsoto',                      # Not used with sqlite3.
-#        'PASSWORD': 'aacs1988',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'indtiendadb',                      # Or path to database file if using sqlite3.
+        'USER': 'andycsoto',                      # Not used with sqlite3.
+        'PASSWORD': 'aacs1988',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
